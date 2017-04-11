012976c7b6a5db4e62d5685a65c7cd97c225ba51
Lenon Maguire <lenon.maguire@gmail.com>
2017-04-10 18:18:23 -0700

    Part one of the Studio is done. Finished part 2.1 also.

--- src/CheeseMVC/Migrations/20170411002009_Mig1.Designer.cs
+++ src/CheeseMVC/Migrations/20170411002009_Mig1.Designer.cs
@@ -8,9 +8,10 @@ using CheeseMVC.Data;
 namespace CheeseMVC.Migrations
 {
     [DbContext(typeof(CheeseDbContext))]
-    partial class CheeseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170411002009_Mig1")]
+    partial class Mig1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
